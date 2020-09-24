2ada54580f6b480a077070057c3e731dc038d0a3
Pearly12 <matt.furlong.12@gmail.com>
2020-09-24 16:09:49 -0400

    test

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -4,7 +4,7 @@
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/background.jpg') ?>);"></div>
       <div class="page-banner__content container t-center c-white">
         <h1 class="headline headline--large">Welcome to Space College!</h1>
-        <h2 class="headline headline--medium">Shoot for the stars!</h2>
+        <h2 class="headline headline--medium">Shoot for the stars</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
         <a href="<?php echo get_post_type_archive_link('program') ?>" class="btn btn--large btn--blue">Find Your Major</a>
       </div>
